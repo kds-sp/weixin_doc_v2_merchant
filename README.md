e59840d25a0ffff0614cb6b358415c1285027c22
dennis <dsk@soundplatform.co.kr>
2025-11-18 12:04:38 +0900

    urls_general_rules.txt 추가

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # weixin pay 분석용
 
+* wechat pay 중국본토 서비스에 적용할 예정
+* 결제는 PC 웹브라우저와 스마트폰 모바일 브라우저만 지원함
+
 * 원본: https://pay.weixin.qq.com/doc/v2/merchant/4011936527
--- wgetAll.sh
+++ wgetAll.sh
@@ -1,9 +1,9 @@
 wget --base=https://pay.weixin.qq.com \
-     --input-file=urls.txt \
+     --input-file=urls_general_rules.txt \
      --wait=1 \
      --random-wait \
      --user-agent="Mozilla/5.0" \
-     --directory-prefix=wechat_docs \
+     --directory-prefix=general_rules \
      --no-clobber \
      --convert-links \
      --adjust-extension
